f023b3e006843affa25c6940aeca36e95e221b92
skaldat1 <skaldat1@binghamton.edu>
2020-04-21 01:05:07 -0400

    build commit

---
+++ build/precache-manifest.e10d97cebab332db62900ec9c469337c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de07d338dd98f078d5fa041f31cafcce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82ba7c61f5ec56737153",
+    "url": "/static/css/2.77a07a6c.chunk.css"
+  },
+  {
+    "revision": "de6b3125e669b7b4e603",
+    "url": "/static/css/main.cd05cd1d.chunk.css"
+  },
+  {
+    "revision": "82ba7c61f5ec56737153",
+    "url": "/static/js/2.12c7ca92.chunk.js"
+  },
+  {
+    "revision": "bbd43f79143413017280698256cad3ab",
+    "url": "/static/js/2.12c7ca92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6b3125e669b7b4e603",
+    "url": "/static/js/main.91d45709.chunk.js"
+  },
+  {
+    "revision": "49ca98a8bb93583bd7b2",
+    "url": "/static/js/runtime-main.d3319aed.js"
+  }
+]);
